c67c33a3c0910df030ab8f81e50ab696807b4f07
asmatalukder3134 <asma.talukder@yahoo.com>
2020-06-15 11:33:37 -0400

    baby

---
+++ collection/src/test/java/allBasic/Overloading.java
@@ -0,0 +1,13 @@
+package allBasic;
+
+public class Overloading {
+
+	public void login(String name,int age) {
+		String n;
+		int a;
+		System.out.println("name is :"+name);
+		System.out.println("age is :"+age);
+
+	}
+
+}
